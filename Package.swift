31680de9847f1d2aa1164852d5d675eb17086e68
Gaurav Vig <gaurav.vig@viewlift.com>
2022-08-10 14:55:39 +0530

    Addition of package for player sdk

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VLPlayerLib",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(name: "VLPlayerLib", targets: ["VLPlayerLib"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(name: "VLPlayerLib", path: "VLPlayerLib.xcframework"),
+        .binaryTarget(name: "GoogleInteractiveMediaAds", path: "DependentFrameworks/GoogleInteractiveMediaAds.xcframework"),
+        .binaryTarget(name: "AmazonIVSPlayer", path: "DependentFrameworks/AmazonIVSPlayer.xcframework"),
+    ]
+)
