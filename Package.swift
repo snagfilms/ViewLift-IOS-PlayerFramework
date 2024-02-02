151f8941431240da71417fd8c44fb9811387b1c8
Rakesh Sharma <rakeshsharma@viewlift.com>
2024-02-02 19:19:31 +0530

    fixes for tvos

--- Package.swift
+++ Package.swift
@@ -5,12 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "VLPlayerLib",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v14),.tvOS(.v14)],
     products: [
         .library(name: "VLPlayerLib", targets: ["VLPlayerLibWrapper"]),
-        .library(name: "GoogleInteractiveMediaAds", targets: ["GoogleInteractiveMediaAds"]),
-        .library(name: "AmazonIVSPlayer", targets: ["AmazonIVSPlayer"]),
-        .library(name: "GoogleCast", targets: ["GoogleCast"]),
+        .library(name: "GoogleInteractiveMediaAds-tvOS", targets: ["GoogleInteractiveMediaAds-tvOS"]),
+        .library(name: "GoogleInteractiveMediaAds-iOS", targets: ["GoogleInteractiveMediaAds-iOS"]),
+        .library(name: "AmazonIVSPlayer-iOS", targets: ["AmazonIVSPlayer-iOS"]),
+        .library(name: "GoogleCast-iOS", targets: ["GoogleCast-iOS"]),
     ],
     dependencies: [
         .package(
@@ -30,17 +31,19 @@ let package = Package(
         .binaryTarget(name: "VLPlayerLib", path: "VLPlayerLib.xcframework"),
         .target(name: "VLPlayerLibWrapper",
                 dependencies: [
-                    .byName(name: "GoogleInteractiveMediaAds"),
-                    .byName(name: "GoogleCast"),
-                    .byName(name: "AmazonIVSPlayer"),
+                    .byName(name: "GoogleInteractiveMediaAds-iOS", condition: .when(platforms: [.iOS])),
+                    .byName(name: "GoogleInteractiveMediaAds-tvOS", condition: .when(platforms: [.tvOS])),
+                    .byName(name: "GoogleCast-iOS", condition: .when(platforms: [.iOS])),
+                    .byName(name: "AmazonIVSPlayer-iOS", condition: .when(platforms: [.iOS])),
                     .product(name: "VLBeaconLib", package: "VLBeaconLib"),
                     .product(name: "VisualEffectView", package: "VisualEffectView"),
                     .product(name: "M3U8Parser", package: "M3U8Parser"),
                     .target(name: "VLPlayerLib")
                 ],
                 path: "VLPlayerLibWrapper/Sources"),
-        .binaryTarget(name: "GoogleInteractiveMediaAds", path: "DependentFrameworks/GoogleInteractiveMediaAds.xcframework"),
-        .binaryTarget(name: "AmazonIVSPlayer", path: "DependentFrameworks/AmazonIVSPlayer.xcframework"),
-        .binaryTarget(name: "GoogleCast", path: "DependentFrameworks/GoogleCast.xcframework"),
+        .binaryTarget(name: "GoogleInteractiveMediaAds-tvOS", path: "DependentFrameworks/tvOS/GoogleInteractiveMediaAds-tvOS.xcframework"),
+        .binaryTarget(name: "GoogleInteractiveMediaAds-iOS", path: "DependentFrameworks/iOS/GoogleInteractiveMediaAds-iOS.xcframework"),
+        .binaryTarget(name: "AmazonIVSPlayer-iOS", path: "DependentFrameworks/iOS/AmazonIVSPlayer-iOS.xcframework"),
+        .binaryTarget(name: "GoogleCast-iOS", path: "DependentFrameworks/iOS/GoogleCast-iOS.xcframework"),
     ]
 )
