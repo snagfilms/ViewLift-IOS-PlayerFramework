5c34a403e4ddf2816ae866d3758258820a9c7f58
AyushiGarg <ayushigarg@viewlift.com>
2025-08-03 21:34:46 +0530

    MediaSDK

--- ViewliftPlayerSampleApp/VideoPlaybackController.swift
+++ ViewliftPlayerSampleApp/VideoPlaybackController.swift
@@ -10,6 +10,8 @@ import UIKit
 import VLPlayerLib
 import VLBeaconLib
 import VLAuthentication
+import VLAnalyticsLib
+import AVKit
 
 // MARK: - Constants
 private enum Constants {
@@ -46,7 +48,9 @@ class VideoPlaybackController: UIViewController {
     var hideControls: Bool = false
     var muteEnabled: Bool = false
     var streamUrl: String?
-    
+    var player: AVPlayer?
+    var currentAdAssetInfo: VLAdAssetInfo?
+    var videoResponse: VLVideoResponseModel?
     // Private Properties
     private var nextVideoLists: [String] = [] {
         didSet {
@@ -106,6 +110,8 @@ class VideoPlaybackController: UIViewController {
     
     private func cleanupResources() {
         vlPlayer?.destroy()
+        vlPlayer?.playerAdsAnalyticsDelegate = nil
+        vlPlayer?.playerVideoAnalyticsDelegate = nil
         videoPlayerControlsView?.removeFromSuperview()
         fullScreenView?.dismiss(animated: false)
     }
@@ -189,6 +195,9 @@ extension VideoPlaybackController {
                 isDVREnabled: isDVREnabled,
                 loader: loader
             )
+            if let contentResponse = contentResponse {
+                self?.parseVLVideoResponse(from: contentResponse)
+            }
         }
     }
     
@@ -219,6 +228,9 @@ extension VideoPlaybackController {
                 isDVREnabled: false,
                 loader: loader
             )
+            if let contentResponse = contentResponse {
+                self?.parseVLVideoResponse(from: contentResponse)
+            }
         }
     }
     
@@ -242,6 +254,8 @@ extension VideoPlaybackController {
         videoPlayerControlsView?.videoPlayer = vlPlayer
         vlPlayer.videoPlayerDelegate = self
         vlPlayer.clientSideAdTrackingDelegate = self
+        vlPlayer.playerAdsAnalyticsDelegate = self
+        vlPlayer.playerVideoAnalyticsDelegate = self
         vlPlayer.enablePlayerBitrateLogs = enableBitrateLogs
     }
     
@@ -365,7 +379,111 @@ extension VideoPlaybackController {
     
     @IBAction private func backButtonClicked(_ sender: Any) {
         vlPlayer.destroy()
+        vlPlayer.playerAdsAnalyticsDelegate = nil
+        vlPlayer.playerVideoAnalyticsDelegate = nil
         dismissViewController()
+    }
+}
+
+extension VideoPlaybackController: VLAnalyticsPlayerClientProtocol {
+
+    func setAdInfo(adId: String, adName: String, podName: String?, podLength: Double?, podPosition: Int?, podOffset: Double?, startTime: Double?) {
+        self.currentAdAssetInfo = VLAdAssetInfo(adId: adId, adName: adName, podName: podName, podLength: podLength, podPosition: podPosition, podOffset: podOffset, startTime: startTime)
+    }
+    
+    
+    func setAdInfo(adId: String, adName: String, podName: String?, podLength: Double?, podPosition: Int?, podOffset: Double?, startTime: Double?, adSystem: String?) {
+        self.currentAdAssetInfo = VLAdAssetInfo(adId: adId, adName: adName, podName: podName, podLength: podLength, podPosition: podPosition, podOffset: podOffset, startTime: startTime, adSystem: adSystem)
+    }
+    
+    func setAdComplete() {
+        self.currentAdAssetInfo = nil
+    }
+    
+    func getAdsInfo() -> VLAdAssetInfo? {
+        return self.currentAdAssetInfo
+    }
+    
+    func parseVLVideoResponse(from dictionary: [String: Any]) {
+        do {
+            // Convert dictionary to JSON Data
+            let jsonData = try JSONSerialization.data(withJSONObject: dictionary, options: [])
+
+            // Decode using JSONDecoder
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .useDefaultKeys
+            let response = try decoder.decode(VLVideoResponseModel.self, from: jsonData)
+
+            self.videoResponse = response
+            
+        } catch {
+            print("❌ Failed to decode from dictionary:", error)
+        }
+    }
+    
+    func getFormattedDateFromTimestamp(timestamp: TimeInterval?) -> String? {
+        guard let timestamp = timestamp else {
+            return nil
+        }
+        let date = Date(timeIntervalSince1970: timestamp)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy" // <- DD/MM/YYYY format
+        formatter.timeZone = .current       // or use .utc for UTC time
+        
+        let formattedDate = formatter.string(from: date)
+        print("Publish Date: \(formattedDate)")
+        return formattedDate
+    }
+    
+    func getVideoInfo() -> VLContentInfo? {
+        var streamType: String = ""
+        var isContentFree: Bool = false
+        var isFullScreen: Bool = false
+        
+        if let mediaTyp = self.videoResponse?.video?.streamingInfo?.isLiveStream {
+            streamType = mediaTyp ? "live" : "vod"
+        }
+
+        if let monetizationModel = self.videoResponse?.video?.monetizationModels {
+            isContentFree = monetizationModel.contains(where: { $0.type == "FREE" })
+        }
+        
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            isFullScreen = appDelegate.isFullScreen
+        }
+        
+        let data = self.videoResponse?.video
+        let isFree = !(self.videoResponse?.plans?.isEmpty ?? false) && isContentFree
+        let status = isFree ? "free" : "premium"
+        let videobroadcast = self.videoResponse?.video?.streamingInfo?.isLiveStream ?? false ? "Broadcast" : "Digital"
+
+        return VLContentInfo(
+            id: data?.id,
+            title: data?.title,
+            seriesTitle: data?.title,
+            contentType: streamType,
+            airDate: self.getFormattedDateFromTimestamp(timestamp: self.videoResponse?.video?.publishDate),
+            streamType: streamType,
+            screenSize: isFullScreen ? "FullScreen" : "Normal",
+            videostatus: status,
+            videotmsid: data?.id,
+            videobroadcast: videobroadcast,
+            videoInitiate: "Manual"
+        )
+    }
+    
+    func trackVideoStart() {
+        let eventBuilder = VLEventModelBuilder()
+            .eventType(.mediaPlay)
+            .contentInfo(getVideoInfo())
+            .tvProviderInfo(VLTVProviderInfo(tvProviderName: /*ParentalControlHelper.getUserDetails()?.mvpdProvider*/ "",
+                                             requestorId:/* AppConfiguration.shared.tveSettings?.requestorId*/ "")
+            )
+            .adsInfo(getAdsInfo())
+            .setPlayer(player)
+            .build()
+        VLAnalytics.shared.trackEvent(data: eventBuilder)
     }
 }
 
@@ -375,6 +493,7 @@ extension VideoPlaybackController: videoPlaybackDelegate {
     func videoStarted(timestamp: Double, playerTag: String) {
         videoPlayerControlsView?.setPlayButtonState(state: true)
         videoPlayerControlsView?.updateTimeLabelOnStart()
+        self.trackVideoStart()
     }
     
     func videoPause(timestamp: Double, playerTag: String) {
@@ -570,6 +689,8 @@ extension VideoPlaybackController {
             DispatchQueue.main.async {
                 AppDelegate.shared.authorizationToken = userIdentity?.authorizationToken
                 self?.vlPlayer.destroy()
+                self?.vlPlayer.playerAdsAnalyticsDelegate = nil
+                self?.vlPlayer.playerVideoAnalyticsDelegate = nil
                 self?.loadPlayerView()
             }
         }
