01c45203087ee44f68079d1b34eba32051268e45
rakesh-vl <rakeshsharma@viewlift.com>
2025-08-04 20:07:33 +0530

    added tv analytics

--- ViewLiftPlayerSampleApp_tvOS/AppDelegate.swift
+++ ViewLiftPlayerSampleApp_tvOS/AppDelegate.swift
@@ -25,6 +25,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.setupAuthentication()
+        
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         let assetVC = AssetListViewController()
         readVideoList(readVideoListOperation: ReadFromLocalJson())
