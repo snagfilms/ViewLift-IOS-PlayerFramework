1bd3b356961311cd0b5e0a17d9f53b419a4c5d55
Shivam Sharma <shivamsharma@viewlift.com>
2025-08-16 15:33:27 +0530

    Sample code changes

---
+++ clientSideAdTrackingEvents.swift
@@ -0,0 +1,8 @@
+//
+//  clientSideAdTrackingEvents.swift
+//  ViewliftPlayerSampleApp
+//
+//  Created by Shivamsharma@viewlift.com on 14/08/25.
+//  Copyright © 2025 Viewlift. All rights reserved.
+//
+
