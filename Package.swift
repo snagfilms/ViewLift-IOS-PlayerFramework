61216549dc69ce0e73e2ee9f2a8687e212d7f631
rakesh-vl <rakeshsharma@viewlift.com>
2025-11-14 14:35:32 +0530

    m3u8 parser fix

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(
             name: "M3U8Parser",
             url: "https://github.com/M3U8Kit/M3U8Parser.git",
-            from: "1.0.2"),
+            Version("1.0.2")..<Version("1.0.2")),
         .package(
             name: "BitmovinPlayer",
             url: "https://github.com/bitmovin/player-ios.git",
