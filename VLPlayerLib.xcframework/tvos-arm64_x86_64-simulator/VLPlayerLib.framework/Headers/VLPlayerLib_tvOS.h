6726742e9e982a9871a0fd0272f72dac237212ab
VikasViewLift <vikassachan@viewlift.com>
2025-12-10 18:27:02 +0530

    framework updated

---
+++ VLPlayerLib.xcframework/tvos-arm64_x86_64-simulator/VLPlayerLib.framework/Headers/VLPlayerLib_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  VLPlayerLib_tvOS.h
+//  VLPlayerLib_tvOS
+//
+//  Created by Rajni Pathak on 31/01/23.
+//  Copyright © 2023 Viewlift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VLPlayerLib_tvOS.
+FOUNDATION_EXPORT double VLPlayerLib_tvOSVersionNumber;
+
+//! Project version string for VLPlayerLib_tvOS.
+FOUNDATION_EXPORT const unsigned char VLPlayerLib_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VLPlayerLib_tvOS/PublicHeader.h>
+
+
