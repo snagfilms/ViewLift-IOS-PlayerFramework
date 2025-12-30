5e4b88e43e5fc5b85f2e0a8d387a87765c983b2f
VikasViewLift <vikassachan@viewlift.com>
2025-12-30 16:13:06 +0530

    Helper function Added

--- CommonClasses/Extensions/UIView+Extension.swift
+++ CommonClasses/Extensions/UIView+Extension.swift
@@ -97,4 +97,15 @@ extension UIView {
         NSLayoutConstraint.activate(constraints)
     }
     
+    var parentViewController: UIViewController? {
+        var responder: UIResponder? = self
+        while let nextResponder = responder?.next {
+            if let viewController = nextResponder as? UIViewController {
+                return viewController
+            }
+            responder = nextResponder
+        }
+        return nil
+    }
+    
 }
