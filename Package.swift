b2dc032891fc5b41a6647fa8bd4c0561c9188c03
VikasViewLift <vikassachan@viewlift.com>
2026-03-17 23:08:21 +0530

    Google ads SDK version set to exact 3.28.10

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/bitmovin/player-ios.git", exact: "3.85.2"),
         .package(url: "https://github.com/snagfilms/iOS-VLBeacon-SPM.git", exact: "3.2.7"),
         .package(url: "https://github.com/muxinc/mux-stats-sdk-avplayer", exact: "4.0.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios.git", from: "3.28.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios.git", exact: "3.28.10"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-tvos.git", from: "4.16.0")
     ],
     targets: [
